4929d4d2e98fd590004ed56adddf0383af6834f3
Marycruz Delgado <marycruz.delgado@hyperiondg.com>
2019-02-19 16:13:03 -0600

    estructura sitio

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>IIEECSA | Instalaciones y mantenimientos electromecánicos</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/general.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/jquery.bxslider.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="css/animate.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <main>
+        <section class="wow slideInUp mb20" id="home">
+          <div class="bxslider">
+            <div><img src="images/slider/slide1.jpg"></div>
+            <div><img src="images/slider/slide2.jpg"></div>
+            <div><img src="images/slider/slide3.jpg"></div>
+          </div>
+        </section>
+        <section class="wow slideInLeft">
+          <div class="bxslider2">
+            <div class="row">
+              <div class="col s12 m6">
+                <h2>Nosotros<span class="c-blue">.</span>.</h2>
+              </div>
+              <div class="col s12 m6 pd0">
+                <img src="images/nosotros.png" class="responsive-img">
+              </div>
+            </div>
+            <div><img src="images/slider/slide2.jpg"></div>
+            <div><img src="images/slider/slide3.jpg"></div>
+          </div>
+        </section>
+      </main>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script src="js/jquery.bxslider.js"></script>
+      <script src="js/wow.min.js"></script>
+      <script type="text/javascript" src="js/init.js"></script>
+    </body>
+  </html>
---
+++ js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	new WOW().init();
+});
+
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: false,
+    slideWidth: 1900
+  });
+  $('.bxslider2').bxSlider({
+    mode: 'fade',
+    captions: false,
+    slideWidth: 1900
+  });
+});
