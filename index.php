8215409a16a672c67b05d7e3017459e5467f4a62
Marycruz Delgado <madet_ixoye@hotmail.com>
2019-02-19 21:30:50 -0600

    add secciones nosotros, mision, vision, equipo y servicios

--- index.php
+++ index.php
@@ -29,14 +29,148 @@
           <div class="bxslider2">
             <div class="row">
               <div class="col s12 m6">
-                <h2>Nosotros<span class="c-blue">.</span>.</h2>
+                <div class="w80 ma mt40">
+                  <h2 class="f-bold">Nosotros<span class="c-blue">.</span></h2>
+                  <p class="fs-16"><span class="c-blue f-bold">IIEECSA</span> es una empresa conformada por profesionales en el desarrollo de la ingeniería eléctrica, mecánica y de construcción.</p>
+                  <p class="fs-16">Brindando apoyo con asesoría integral para la elaboración de proyectos y obras de la industria en general, considerando en todo momento una solución de Costo-Beneficio para nuestros clientes. Se ha habilitado la compatibilidad con lectores de pantalla.</p>
+                </div>
               </div>
               <div class="col s12 m6 pd0">
                 <img src="images/nosotros.png" class="responsive-img">
               </div>
             </div>
-            <div><img src="images/slider/slide2.jpg"></div>
-            <div><img src="images/slider/slide3.jpg"></div>
+            <div class="row">
+              <div class="col s12 m6 pd0">
+                <img src="images/mision.png" class="responsive-img">
+              </div>
+              <div class="col s12 m6">
+                <div class="w80 ma mt40">
+                  <h2 class="f-bold">Misión<span class="c-blue">.</span></h2>
+                  <p class="fs-16"><span class="c-blue f-bold">Ofrecer servicios de calidad,</span> satisfaciendo las necesidades de nuestros clientes y estableciendo relaciones duraderas.</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12 m6">
+                <div class="w80 ma mt40">
+                  <h2 class="f-bold">Visión<span class="c-blue">.</span></h2>
+                  <p class="fs-16">Ser una <span class="c-blue f-bold">empresa líder</span> en el mercado, apoyando al crecimiento económico de México.</p>
+                </div>
+              </div>
+              <div class="col s12 m6 pd0">
+                <img src="images/vision.png" class="responsive-img">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12 m6 pd0">
+                <img src="images/equipo.png" class="responsive-img">
+              </div>
+              <div class="col s12 m6">
+                <div class="w80 ma mt40">
+                  <h2 class="f-bold">Equipo<span class="c-blue">.</span></h2>
+                  <p class="fs-16">Contamos con <span class="c-blue f-bold">ingenieros, técnicos y personal administrativo</span>, para la ejecución de los servicios que ofrecemos; así como equipo de transporte, herramienta, equipo de oficina, taller y almacén para ofrecer a nuestros clientes la mejor atención de una manera eficiente y oportuna.</p>
+                  <p class="fs-16">Nuestra capacidad financiera nos permite <span class="c-blue f-bold">solventar proyectos de gran presupuesto.</span></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="wow slideInRight bg_blue white-text">
+          <div class="row">
+            <div class="col s12 center-align">
+              <h2 class="f-bold">SERVICIOS.</h2>
+            </div>
+          </div>
+          <div class="w90 ma">
+            <div class="row">
+              <div class="col s12 m3">
+                <img src="images/servicios/instalaciones_01.png" class="responsive-img">
+                <h4 class="f-bold">INSTALACIONES.</h4>
+                <ul>
+                  <li>· Aire acondicionado</li>
+                  <li>· Oficinas</li>
+                  <li>· Industria</li>
+                  <li>· Casa/Habitación</li>
+                  <li>· Centro de computo</li>
+                  <li>· Eléctricas en alta y baja tensión</li>
+                  <li>· Hidráulicas y sanitarias</li>
+                  <li>· Vidrio y cancelería de aluminio</li>
+                </ul>
+              </div>
+              <div class="col s12 m3">
+                <img src="images/servicios/mantenimiento_02.png" class="responsive-img">
+                <h4 class="f-bold">MANTENIMIENTO.</h4>
+                <ul>
+                  <li>· Equipos de aire acondicionado</li>
+                  <li>· Instalaciones eléctricas</li>
+                  <li>· Plantas eléctricas de emergencia</li>
+                  <li>· Redes hidráulicas</li>
+                  <li>· Instalaciones de iluminación</li>
+                  <li>· Inmuebles</li>
+                  <li>· Equipos de refrigeración</li>
+                  <li>· Cisternas</li>
+                </ul>
+              </div>
+              <div class="col s12 m3">
+                <img src="images/servicios/venta_03.png" class="responsive-img">
+                <h4 class="f-bold">VENTA.</h4>
+                <ul>
+                  <li>· Aires acondicionados</li>
+                  <li>· Refacciones mecánicas y de aire acondicionado</li>
+                  <li>· Material eléctrico de media y baja tensión</li>
+                  <li>· Equipos de aire acondicionado</li>
+                </ul>
+              </div>
+              <div class="col s12 m3">
+                <img src="images/servicios/plantas_04.png" class="responsive-img">
+                <h4 class="f-bold">PLANTAS DE EMERGENCIA.</h4>
+                <ul>
+                  <li>· Mantenimiento preventivo y correctivo</li>
+                  <li>· Venta</li>
+                </ul>
+              </div>
+            </div>
+            <div class="row mb0">
+              <div class="col s12 m3">
+                <img src="images/servicios/aire_05.png" class="responsive-img">
+                <h4 class="f-bold">AIRE ACONDICIONADO..</h4>
+                <ul>
+                  <li>· Aire acondicionado y refrigeración</li>
+                  <li>· Aire acondicionado de precisión</li>
+                  <li>· Ventilación</li>
+                </ul>
+              </div>
+              <div class="col s12 m3">
+                <img src="images/servicios/obra_06.png" class="responsive-img">
+                <h4 class="f-bold">Ventilación.</h4>
+                <ul>
+                  <li>· Hidrosanitaria</li>
+                  <li>· Canceleria</li>
+                  <li>· Remodelación</li>
+                  <li>· Arquitectónicos</li>
+                  <li>· Diseño de interiores</li>
+                  <li>· Impermeabilización</li>
+                </ul>
+              </div>
+              <div class="col s12 m3">
+                <img src="images/servicios/subestación_07.png" class="responsive-img">
+                <h4 class="f-bold">SUBESTACIONES ELÉCTRICAS.</h4>
+                <ul>
+                  <li>· Pararayos</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="wow slideInUp black">
+          <div class="container">
+            <div class="row mb0">
+              <div class="col s12 center-align white-text">
+                <p class="fs-12">Eléctricos en alta y baja tensión - Banco de capacitores - Sistema de puesta a tierra - Ahorro de energía - Hidráulicos y sanitarios - Integrales para centros de cómputo - Sistema de fuerza ininterrumpible - Análisis de armónicas</p>
+              </div>
+            </div>
           </div>
         </section>
       </main>
