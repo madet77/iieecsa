7b9fd201b66fe5b2a199d590943c7efc37007100
Marycruz Delgado <madet_ixoye@hotmail.com>
2019-02-20 23:25:03 -0600

    sitio web completo

--- index.php
+++ index.php
@@ -5,27 +5,45 @@
       <title>IIEECSA | Instalaciones y mantenimientos electromecánicos</title>
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link type="text/css" rel="stylesheet" href="fonts/icons/style.css"  media="screen,projection"/>
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="css/general.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="css/jquery.bxslider.css"  media="screen,projection"/>
       <link rel="stylesheet" href="css/animate.css">
 
+      <!-- Owl Stylesheets -->
+      <link rel="stylesheet" href="css/owl.carousel.min.css">
+      <link rel="stylesheet" href="css/owl.theme.default.min.css">
+      <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
     <body>
+
+      <nav class="white">
+        <div class="nav-wrapper w90 ma">
+          <a href="#" class="brand-logo f-bold c-blue">IIEECSA.</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#home" class="c-blue">Home</a></li>
+            <li><a href="#quienesSomos" class="c-blue">¿Quiénes somos?</a></li>
+            <li><a href="#servicios" class="c-blue">Servicios</a></li>
+            <li><a href="#clientes" class="c-blue">Clientes</a></li>
+            <li><a href="#contacto" class="c-blue">Contacto</a></li>
+          </ul>
+        </div>
+      </nav>
+        
       <main>
-        <section class="wow slideInUp mb20" id="home">
+        <section class="wow slideInUp mb20 scrollspy" id="home">
           <div class="bxslider">
             <div><img src="images/slider/slide1.jpg"></div>
             <div><img src="images/slider/slide2.jpg"></div>
             <div><img src="images/slider/slide3.jpg"></div>
           </div>
         </section>
-        <section class="wow slideInLeft">
+        <section class="wow slideInLeft scrollspy" id="quienesSomos">
           <div class="bxslider2">
             <div class="row">
               <div class="col s12 m6">
@@ -76,13 +94,13 @@
           </div>
         </section>
 
-        <section class="wow slideInRight bg_blue white-text">
-          <div class="row">
+        <section class="bg_blue white-text scrollspy mb" id="servicios">
+          <div class="row wow slideInLeft">
             <div class="col s12 center-align">
               <h2 class="f-bold">SERVICIOS.</h2>
             </div>
           </div>
-          <div class="w90 ma">
+          <div class="w90 ma wow slideInRight">
             <div class="row">
               <div class="col s12 m3">
                 <img src="images/servicios/instalaciones_01.png" class="responsive-img">
@@ -164,21 +182,133 @@
           </div>
         </section>
 
-        <section class="wow slideInUp black">
+        <section class="black">
           <div class="container">
-            <div class="row mb0">
+            <div class="row mb0 wow slideInUp">
               <div class="col s12 center-align white-text">
                 <p class="fs-12">Eléctricos en alta y baja tensión - Banco de capacitores - Sistema de puesta a tierra - Ahorro de energía - Hidráulicos y sanitarios - Integrales para centros de cómputo - Sistema de fuerza ininterrumpible - Análisis de armónicas</p>
               </div>
             </div>
           </div>
         </section>
+        <section class="scrollspy mb20" id="clientes">
+          <div class="row">
+            <div class="col s12 center-align">
+              <h2 class="f-bold">CLIENTES<span class="c-blue">.</span></h2>
+            </div>
+          </div>
+          <div class="w90 ma wow slideInLeft">
+            <div class="row">
+              <div class="col s12">
+                <!-- Set up your HTML -->
+                <div class="owl-carousel owl-theme">
+                  <div><img src="images/clientes/01.png"></div>
+                  <div><img src="images/clientes/02.png"></div>
+                  <div><img src="images/clientes/03.png"></div>
+                  <div><img src="images/clientes/04.png"></div>
+                  <div><img src="images/clientes/05.png"></div>
+                  <div><img src="images/clientes/06.png"></div>
+                  <div><img src="images/clientes/07.png"></div>
+                  <div><img src="images/clientes/09.png"></div>
+                  <div><img src="images/clientes/09.png"></div>
+                  <div><img src="images/clientes/10.png"></div>
+                  <div><img src="images/clientes/11.png"></div>
+                  <div><img src="images/clientes/12.png"></div>
+                  <div><img src="images/clientes/13.png"></div>
+                  <div><img src="images/clientes/14.png"></div>
+                  <div><img src="images/clientes/15.png"></div>
+                  <div><img src="images/clientes/16.png"></div>
+                  <div><img src="images/clientes/17.png"></div>
+                  <div><img src="images/clientes/18.png"></div>
+                  <div><img src="images/clientes/19.png"></div>
+                  <div><img src="images/clientes/20.png"></div>
+                  <div><img src="images/clientes/21.png"></div>
+                  <div><img src="images/clientes/22.png"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="scrollspy black white-text" id="contacto">
+          <div class="row wow slideInRight">
+            <div class="col s12 center-align">
+              <h2 class="f-bold">CONTACTO<span class="c-blue">.</span></h2>
+            </div>
+          </div>
+          <div class="w90 ma wow slideInUp">
+            <div class="row mb0">
+              <form class="col s12 m6">
+                <div class="row">
+                  <div class="col s12">
+                    <h3 class="f-bold mt0">¡Escríbenos!</h3>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12">
+                    <input placeholder="Nombre" id="first_name" type="text" class="validate">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12 m6">
+                    <input placeholder="Teléfono" id="last_name" type="text" class="validate">
+                  </div>
+                  <div class="col s12 m6">
+                    <input placeholder="Email" id="last_name" type="text" class="validate">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12">
+                    <textarea id="textarea1" class="materialize-textarea" placeholder="Mensaje"></textarea>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12 center-align">
+                    <button class="btn bg_blue white-text w100 brad-20" type="submit">Enviar</button>
+                  </div>
+                </div>
+              </form>
+              <div class="col s12 m6">
+                <h3 class="f-bold mt0">OFICINAS<span class="c-blue">.</span></h3>
+                <ul class="ul-oficinas">
+                  <li><h4>Teléfonos.</h4></li>
+                  <li><i class="material-icons left">call</i> 5421 5172</li>
+                  <li><i class="material-icons left">chevron_right</i>5617 2380</li>
+                  <li><i class="icon-whatsapp_icon left fs-18"></i> 55 4324 9811</li>
+                </ul>
+                <ul class="ul-oficinas">
+                  <li><h4>Email.</h4></li>
+                  <li><i class="material-icons left">chevron_right</i>contacto@iieecsa.com</li>
+                  <li><i class="material-icons left">chevron_right</i>ventas@iieecsa.com</li>
+                </ul>
+                <div class="row">
+                  <div class="col s12">
+                    <h5 class="white-text">¡Síguenos!</h5>
+                    <ul class="ul-social">
+                      <li><a class="grey-text text-lighten-3 btn-round" href="#!"><span class="icon-facebook_icon fs-30"></span></a></li>
+                      <li><a class="grey-text text-lighten-3 btn-round" href="#!"><span class="icon-Instagrma_icon fs-30"></span></a></li>
+                      <li><a class="grey-text text-lighten-3 btn-round" href="#!"><span class="icon-twitter_icon fs-30"></span></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
+      <footer class="page-footer black white-text br-top">
+          <div class="footer-copyright">
+            <div class="container center-align">
+              <a class="grey-text text-lighten-4" href="#!">Aviso de Privacidad</a>
+            </div>
+          </div>
+      </footer>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
       <script src="js/jquery.bxslider.js"></script>
       <script src="js/wow.min.js"></script>
+      <script type="text/javascript" src="js/owl.carousel.min.js"></script>
       <script type="text/javascript" src="js/init.js"></script>
     </body>
   </html>
--- js/init.js
+++ js/init.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 	new WOW().init();
+  $('.scrollspy').scrollSpy({
+    scrollOffset:60
+  });
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:20,
+      dots:true,
+      autoplay:true,
+      nav:false,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:3,
+              nav:true
+          },
+          600:{
+              items:6,
+              nav:false
+          },
+          1000:{
+              items:6,
+              nav:true,
+              loop:false
+          }
+      }
+  })
 });
 
 $(function(){
